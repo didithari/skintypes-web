236793da2be2d4fc9bc29e5b67ba15b1b38fa140
tegarmalik <124681117+didithari@users.noreply.github.com>
2026-05-28 01:38:24 +0700

    fix: update prediction API endpoint in CameraController

--- app/Http/Controllers/CameraController.php
+++ app/Http/Controllers/CameraController.php
@@ -60,7 +60,7 @@ class CameraController extends Controller
                 'image',
                 file_get_contents(storage_path("app/public/$path")),
                 'photo.jpg'
-            )->post('http://70.153.24.9:5000/predict');
+            )->post('http://172.188.80.185:5000/predict');
 
             if ($response->successful()) {
                 $result = $response->json();
